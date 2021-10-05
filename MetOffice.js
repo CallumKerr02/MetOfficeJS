647f297478cf5bae148bd2c392b462c412bdb1e0
CallumKerr02 <callumkerr3@hotmail.com>
2021-10-05 12:19:30 +0100

    Fixed weather details, more work to be done :)

--- MetOffice.js
+++ MetOffice.js
@@ -1,55 +1,57 @@
 // Met Office API Task :)
 
-const got = require('got');
+const got = require("got");
 
-const prompt = require('prompt-sync')();
+const prompt = require("prompt-sync")();
 
-const APIKey = '6e603ffc-5522-49d3-aa02-094f23c9f4fb';
+const APIKey = "6e603ffc-5522-49d3-aa02-094f23c9f4fb";
 
-class locationName{
-    constructor(name, id){
-        this.name = name;
-        this.id = id;
-    }
+class locationName {
+  constructor(name, id) {
+    this.name = name;
+    this.id = id;
+  }
 }
 
 const grabPromise = async () => {
-    got.get(    // async to this code, get this info.
-            "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=" + (APIKey)
-        )
-        .json()
-        .then((mainBody => {   // After .get is achieved, this code below is then run. 
-            let name = [];
-            for (let a of mainBody.Locations.Location){
-                name.push(a);
-            }
-            return name;
-            console.log(name);
-
-        })).then(( name => {
-            for (const printingLocation of name){
-                console.log(printingLocation["name"]);
-            }
-            const userInput = prompt("Enter Location Name: ");
-            for (const location of name){
-                if (userInput.toLowerCase() === location["name"].toLowerCase()){
-                    got.get(
-                        "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/" + location["id"] + "?res=daily&key=" + (APIKey)
-                    ).json()
-                    .then(( body => {
-                        console.log(body.SiteRep.Wx.Param)
-                    }))
-                    break;
-                }
-                }
-                
-            }
-            
-            
-        ));
-
-    
-
-}
+  got
+    .get(
+      // async to this code, get this info.
+      "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=" +
+        APIKey
+    )
+    .json()
+    .then((mainBody) => {
+      // After .get is achieved, this code below is then run.
+      let name = [];
+      for (let a of mainBody.Locations.Location) {
+        name.push(a);
+      }
+    //   console.log(name);
+      return name;
+    })
+    .then((name) => {
+      for (const printingLocation of name) {
+        console.log(printingLocation["name"]);
+      }
+      const userInput = prompt("Enter Location Name: ");
+      for (const location of name) {
+        if (userInput.toLowerCase() === location["name"].toLowerCase()) {
+          got
+            .get(
+              "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/" +
+                location["id"] +
+                "?res=daily&key=" +
+                APIKey
+            )
+            .json()
+            .then((body) => {
+              console.log(body.SiteRep.DV.Location);
+            });
+          break;
+        }
+      }
+    });
+};
 
 grabPromise();
