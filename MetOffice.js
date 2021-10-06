1cec3d33b02ceb8685bf06f1a288629745fe33c7
Tom Young <tom.young@bp.com>
2021-10-06 15:32:51 +0100

    Wednesday update

--- MetOffice.js
+++ MetOffice.js
@@ -1,27 +1,29 @@
 // Met Office API Task :)
 
 const got = require("got");
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const prompt = require("prompt-sync")();
 const APIKey = "6e603ffc-5522-49d3-aa02-094f23c9f4fb";
 
-app.get('/forecast', function (req, res) {
-  res.send('Hello World');
-})
-app.use(express.static('frontend'));
- 
-app.listen(3000, function(){
-  console.log('Webiste Created');
+app.get("/forecast", function (req, res) {
+  got
+    .get(
+      "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/" +
+        req.query.id +
+        "?res=daily&key=" +
+        APIKey
+    )
+    .json()
+    .then((body) => {
+      res.send(body.SiteRep.DV.Location);
+    });
+});
+app.use(express.static("frontend"));
 
-})
-
-class locationName {
-  constructor(name, id) {
-    this.name = name;
-    this.id = id;
-  }
-}
+app.listen(3000, function () {
+  console.log("Webiste Created");
+});
 
 const grabPromise = async () => {
   got
@@ -37,28 +39,31 @@ const grabPromise = async () => {
       for (let a of mainBody.Locations.Location) {
         name.push(a);
       }
-    //   console.log(name);
+      //   console.log(name);
       return name;
     })
     .then((name) => {
       for (const printingLocation of name) {
         console.log(printingLocation["name"]);
       }
-      const userInput = prompt("Enter Location Name: ");
-      for (const location of name) {
-        if (userInput.toLowerCase() === location["name"].toLowerCase()) {
-          got
-            .get(
-              "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/" +
-                location["id"] +
-                "?res=daily&key=" +
-                APIKey
-            )
-            .json()
-            .then((body) => {
-              console.log(body.SiteRep.DV.Location);
-            });
-          break;
+      locSearch();
+      function locSearch() {
+        const userInput = prompt("Enter Location Name: ");
+        for (const location of name) {
+          if (userInput.toLowerCase() === location["name"].toLowerCase()) {
+            got
+              .get(
+                "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/" +
+                  location["id"] +
+                  "?res=daily&key=" +
+                  APIKey
+              )
+              .json()
+              .then((body) => {
+                console.log(body.SiteRep.DV.Location);
+              });
+            break;
+          }
         }
       }
     });
