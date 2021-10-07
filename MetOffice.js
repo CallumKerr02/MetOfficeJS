3ae297eb21188a45b894a036a3122e1f1bc41ac6
Tom Young <tom.young@bp.com>
2021-10-07 10:35:18 +0100

    Client side started

--- MetOffice.js
+++ MetOffice.js
@@ -19,54 +19,46 @@ app.get("/forecast", function (req, res) {
       res.send(body.SiteRep.DV.Location);
     });
 });
+
+app.get("/siteList", function (req, res) {
+  got
+    .get(
+      "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=" +
+        APIKey
+    )
+    .json()
+    .then((body) => {
+      res.send(body.Locations.Location);
+    });
+});
 app.use(express.static("frontend"));
 
 app.listen(3000, function () {
   console.log("Webiste Created");
 });
 
-const grabPromise = async () => {
-  got
-    .get(
-      // async to this code, get this info.
-      "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=" +
-        APIKey
-    )
-    .json()
-    .then((mainBody) => {
-      // After .get is achieved, this code below is then run.
-      let name = [];
-      for (let a of mainBody.Locations.Location) {
-        name.push(a);
-      }
-      //   console.log(name);
-      return name;
-    })
-    .then((name) => {
-      for (const printingLocation of name) {
-        console.log(printingLocation["name"]);
-      }
-      locSearch();
-      function locSearch() {
-        const userInput = prompt("Enter Location Name: ");
-        for (const location of name) {
-          if (userInput.toLowerCase() === location["name"].toLowerCase()) {
-            got
-              .get(
-                "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/" +
-                  location["id"] +
-                  "?res=daily&key=" +
-                  APIKey
-              )
-              .json()
-              .then((body) => {
-                console.log(body.SiteRep.DV.Location);
-              });
-            break;
-          }
-        }
-      }
-    });
-};
 
-grabPromise();
+
+
+//       locSearch();
+//       function locSearch() {
+//         const userInput = prompt("Enter Location Name: ");
+//         for (const location of siteList) {
+//           if (userInput.toLowerCase() === location["name"].toLowerCase()) {
+//             got
+//               .get(
+//                 "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/" +
+//                   location["id"] +
+//                   "?res=daily&key=" +
+//                   APIKey
+//               )
+//               .json()
+//               .then((body) => {
+//                 console.log(body.SiteRep.DV.Location);
+//               });
+//             break;
+//           }
+//         }
+//       }
+//     });
+// };
---
+++ frontEnd/homepage.js
@@ -0,0 +1,26 @@
+
+const APIKey = "6e603ffc-5522-49d3-aa02-094f23c9f4fb";
+let sites = {};
+// document.querySelector("userInput").innerHTML = `...`;
+
+window.onload = function () {
+  fetch("/siteList")
+    .then((response)=> response.json())
+
+    .then((object) => {
+      sites = object;
+    });
+};
+
+function locationSearch() {
+  const userInput = document.getElementById("userInput").value;
+  for (const location of sites) {
+    if (userInput === location.name) {
+      fetch("/forecast?id=" + location.id)
+        .json() // didn't like it
+        .then((object) => {
+          document.querySelector("#result").innerHTML = "..."; // could be getelementbyid
+        });
+    }
+  }
+}
